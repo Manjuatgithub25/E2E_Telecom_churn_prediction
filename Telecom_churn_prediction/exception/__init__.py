a7145f064076a6f71b0fe952bd29638f60b0ad13
Manjuatgithub25 <manjunathyashram5@gmail.com>
2025-08-14 12:47:51 +0530

    updated custom exception code

--- Telecom_churn_prediction/exception/__init__.py
+++ Telecom_churn_prediction/exception/__init__.py
@@ -1,18 +1,23 @@
 import sys
 
-def error_message_details(error, error_details:sys):
-    _, _, exc_table = error_details.exc_info()
-    file_name = exc_table.tb_frame.f_code.co_filename
-    line_no = exc_table.tb_lineno
-    error_message = "The error is occured in python file {0} at line number {1}: that is {2}".format(file_name, line_no, str(error))
+def error_message_details(error, error_details: sys):
+    exc_type, exc_value, exc_tb = error_details.exc_info()
+    if exc_tb is not None:
+        file_name = exc_tb.tb_frame.f_code.co_filename
+        line_no = exc_tb.tb_lineno
+    else:
+        file_name = "Unknown file"
+        line_no = "Unknown line"
+    error_message = "The error is occured in python file {0} at line number {1}: that is {2}".format(
+        file_name, line_no, str(error)
+    )
     return error_message
 
 
 class CustomException(Exception):
-    def __init__(self, error_message, error_details:sys):
-        super().__init__(error_message)
+    def __init__(self, error_message, error_details: sys):
+        super().__init__(str(error_message))
         self.error_message = error_message_details(error_message, error_details=error_details)
 
-    
     def __str__(self):
         return self.error_message
